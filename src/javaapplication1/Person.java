b56a0af6b1a47acff91b401d71b4913406f33bf7
Rucha Chauthai <ruchachauthai@Ruchas-MBP.hsd1.ma.comcast.net>
2021-09-26 23:57:53 -0400

    Asg

---
+++ src/javaapplication1/Person.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author ruchachauthai
+ */
+public class Person {
+    
+    private String name;
+    private String address;
+    private String dob;
+    private String phnNo;
+    private String faxNo;
+    private String email;
+    private String ssn;
+    private String medicalRecNo;
+    private String healthPlanNo;
+    private String bankAccNo;
+    private String licenseNo;
+    private String vehicleId;
+    private String deviceId;
+    private String linkedIn;
+    private String IPAddress;
+    private String biometricID;
+    private String profilePic;
+    private String uniqueID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhnNo() {
+        return phnNo;
+    }
+
+    public void setPhnNo(String phnNo) {
+        this.phnNo = phnNo;
+    }
+
+    public String getFaxNo() {
+        return faxNo;
+    }
+
+    public void setFaxNo(String faxNo) {
+        this.faxNo = faxNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecNo() {
+        return medicalRecNo;
+    }
+
+    public void setMedicalRecNo(String medicalRecNo) {
+        this.medicalRecNo = medicalRecNo;
+    }
+
+    public String getHealthPlanNo() {
+        return healthPlanNo;
+    }
+
+    public void setHealthPlanNo(String healthPlanNo) {
+        this.healthPlanNo = healthPlanNo;
+    }
+
+    public String getBankAccNo() {
+        return bankAccNo;
+    }
+
+    public void setBankAccNo(String bankAccNo) {
+        this.bankAccNo = bankAccNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.IPAddress = IPAddress;
+    }
+
+    public String getBiometricID() {
+        return biometricID;
+    }
+
+    public void setBiometricID(String biometricID) {
+        this.biometricID = biometricID;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(String uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+    
+    
+}
